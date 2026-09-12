1804cd6cd9dbc8dcefaaf6e3d89ceff9d1c21bbe
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2026-09-12 20:05:54 +0600

    fixing technology issue

--- src/component/Technologies.tsx
+++ src/component/Technologies.tsx
@@ -8,7 +8,9 @@ export interface TechnologiesProps {
   technologiesPromise: Promise<Technology[]>;
 }
 
-export default function Technologies({ technologiesPromise }: TechnologiesProps) {
+export default function Technologies({
+  technologiesPromise,
+}: TechnologiesProps) {
   const allTechnologies = use(technologiesPromise);
 
   const [yourStack, setYourSack] = useState<Technology[]>([]);
@@ -16,19 +18,34 @@ export default function Technologies({ technologiesPromise }: TechnologiesProps)
   return (
     <>
       <div className="max-w-5/6 mx-auto">
+        {/* text tee technology  */}
+
         <div>
-          <h1 className="text-4xl font-bold">Explore the <span className="text-[#D91B7E] ">Technologies</span></h1>
-          <p className="text-[#64748B]">Pick one technology per category to build your ideal stack.</p>
+          <h1 className="text-4xl font-bold">
+            Explore the <span className="text-[#D91B7E] ">Technologies</span>
+          </h1>
+          <p className="text-[#64748B]">
+            Pick one technology per category to build your ideal stack.
+          </p>
         </div>
-        <div className="grid grid-cols-1 gap-3 mt-5 lg:grid-cols-12 ">
-          <div className="grid-cols-3 items-center justify-center gap-8 lg:grid col-span-9">
+
+        {/* all cards  */}
+        <div className="mt-8 grid grid-cols-1 gap-6 lg:grid-cols-12">
+          {/* All Technologies */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-9 lg:grid-cols-3">
             {allTechnologies.map((technology) => (
-              <TechCard key={technology.id} yourStack={yourStack} setYourSack={setYourSack} technology={technology} />
+              <TechCard
+                key={technology.id}
+                yourStack={yourStack}
+                setYourSack={setYourSack}
+                technology={technology}
+              />
             ))}
           </div>
-          {/* right nav bar  */}
-          <div className="col-span-3 ">
-            <YourStack setYourStack={setYourSack} yourStack={yourStack}></YourStack>
+
+          {/* Your Stack */}
+          <div className="w-full lg:col-span-3">
+            <YourStack setYourStack={setYourSack} yourStack={yourStack} />
           </div>
         </div>
       </div>
